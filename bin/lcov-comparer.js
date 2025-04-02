ac606a2619a20d7999190862fa8daf4f9fb9ace4
Emil Yangirov <yangirovme@gmail.com>
2025-04-02 12:19:08 +0300

    chore: add threshold and out options to cli

--- bin/lcov-comparer.js
+++ bin/lcov-comparer.js
@@ -1,13 +1,33 @@
 #!/usr/bin/env node
 
-const path = require("path");
-const { run } = require("../lib/compare");
+const path = require('path');
+const { run } = require('../lib/compare');
 
-const [, , fileA, fileB] = process.argv;
+const args = process.argv.slice(2);
 
-if (!fileA || !fileB) {
-  console.error("Usage: lcov-comparer <lcov-old.info> <lcov-new.info>");
+if (args.length < 2) {
+  console.error(
+    'Usage: lcov-comparer <lcov-old.info> <lcov-new.info> [--out file] [--threshold n]'
+  );
+
   process.exit(1);
 }
 
-run(path.resolve(fileA), path.resolve(fileB));
+const fileA = args[0];
+const fileB = args[1];
+
+let outFile = 'coverage-diff.html';
+let threshold = null;
+
+for (let i = 2; i < args.length; i++) {
+  const arg = args[i];
+  if (arg === '--out' && args[i + 1]) {
+    outFile = args[i + 1];
+    i++;
+  } else if (arg === '--threshold' && args[i + 1]) {
+    threshold = parseFloat(args[i + 1]);
+    i++;
+  }
+}
+
+run(path.resolve(fileA), path.resolve(fileB), { outFile, threshold });
